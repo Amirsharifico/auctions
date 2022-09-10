37932cf34876e43cf1cfc1d6ee068e2d208f0030
Amir <amirsharifico@gmail.com>
2022-09-09 23:00:35 -0400

    12:25 min video4of17

--- auctions-client/src/App.js
+++ auctions-client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import axios from 'axios';
+import {useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
+
+useEffect(()=>{
+  axios.get("http://localhost:3002/auctions").then((response)=>{
+    console.log(response);
+  })
+}, [])
+
+
+  return  <div className="App"></div>
+   
      
-    </div>
-  );
+    
+  
 }
 
 export default App;
